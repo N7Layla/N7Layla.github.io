cbb6a3aebb616916811a5c47587b4ba735394c3c
Layla Hedges <layla.hedges@gmail.com>
2023-03-08 14:53:56 -0500

    Started adding project ARTmood

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare, faCode } from '@fortawesome/free-solid-svg-icons';
+import artmood from '../images/artmood.PNG';
 
 function Projects(props) {  
     return <div className="text-content">
       <div className="project-section">
-        <img src=" " alt="" className="" />
+        <img src={artmood} alt="Screenshot of ARTmood project" className="project-shot" />
         <span>
           <h2>ARTmood</h2> <FontAwesomeIcon icon={faCode} /> <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> 
           <h3>react | redux</h3>
-          originally a project i made during my time at the grace hopper program, ARTmood is a simple web app where the user can search for works of art based on their mood or by a particular color. 
+          originally a project i made during my time at the grace hopper program, ARTmood is a simple web app where the user can search for works of art based on their mood or by a particular color. relaunch of live site coming soon!
           </span>
       </div>
       </div>
